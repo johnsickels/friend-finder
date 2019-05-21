be845107405bdfbc17d394b93ed33b28789f5a04
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-05-21 13:09:15 -0400

    results and modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var friendsData = require("../data/friends");
+var difference;
 
 
 // Your apiRoutes.js file should contain two routes:
@@ -12,31 +13,43 @@ module.exports = function (app) {
     // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function (req, res) {
 
-
-
         var totalDifference = [];
         for (var k = 0; k < friendsData.length; k++) {
 
-            var difference = 0;
+            difference = 0;
 
             for (var i = 0; i < friendsData[k].scores.length; i++) {
                 difference += (Math.abs(req.body.scores[i] - friendsData[k].scores[i]));
-
             };
 
-            console.log(difference);
+            console.log("Difference from " + friendsData[k].name + ": " + difference);
+
             totalDifference.push(difference);
 
-
         };
-        console.log(totalDifference);
+    
+        var smallestDifference = totalDifference.sort(function(a, b){return a-b})[0];
+
+        console.log("Smallest difference: " + smallestDifference);
+        
+
+        for (var k = 0; k < friendsData.length; k++) {
+            difference = 0;
+            for (var i = 0; i < friendsData[k].scores.length; i++) {
+                difference += (Math.abs(req.body.scores[i] - friendsData[k].scores[i]));
+            };
+            if (difference === smallestDifference) {
+                console.log("Friend found: " + friendsData[k].photo);
+                var friendFound = friendsData[k].name;
+                var photoFound = friendsData[k].photo;
+                res.json(friendsData[k]);
+                
+            };
+        }
 
         friendsData.push(req.body);
-        res.json(true);
 
-
-
-
+        
 
     });
 };
