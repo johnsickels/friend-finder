fd745e2f09f7321dcd1d17ceff15c474586062e9
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-05-21 10:40:58 -0400

    calculate totalDifference on all friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,84 @@
-var friendsArray = [];
+var friendsArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Tony",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/c/cb/Robert_Downey_Jr._as_Iron_Man_in_Avengers_Infinity_War.jpg",
+        "scores": [
+            1,
+            5,
+            3,
+            2,
+            1,
+            5,
+            4,
+            1,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Rufus",
+        "photo": "https://cdn10.phillymag.com/wp-content/uploads/sites/3/2018/01/rufus-wainwright-1-900x600.jpg",
+        "scores": [
+            2,
+            4,
+            3,
+            3,
+            2,
+            4,
+            5,
+            2,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Layla",
+        "photo": "http://laylaclaire.com/wp-content/uploads/2010/09/LaylaClaire_Credit_LisaMarieMazzucco-791x1024.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Gennie",
+        "photo": "https://www.globalfundforwomen.org/wp-content/uploads/2015/09/gennie.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,42 @@
 var friendsData = require("../data/friends");
 
+
 // Your apiRoutes.js file should contain two routes:
-module.exports = function(app) {
+module.exports = function (app) {
 
     // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
 
     // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
+
+
+
+        var totalDifference = [];
+        for (var k = 0; k < friendsData.length; k++) {
+
+            var difference = 0;
+
+            for (var i = 0; i < friendsData[k].scores.length; i++) {
+                difference += (Math.abs(req.body.scores[i] - friendsData[k].scores[i]));
+
+            };
+
+            console.log(difference);
+            totalDifference.push(difference);
+
+
+        };
+        console.log(totalDifference);
+
         friendsData.push(req.body);
         res.json(true);
-        console.log("app.post in apiroutes");
-        
-        console.log(req.body);
-        console.log(res.body);
-        console.log(friendsData);
-        
+
+
+
+
+
     });
 };
